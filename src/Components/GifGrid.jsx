4e977a4c7b6b3bfb899322eef87e1f0bd16cede1
claudia <claudiafe@yahoo.com>
2023-02-23 13:02:29 -0300

    Primer commit

---
+++ src/Components/GifGrid.jsx
@@ -0,0 +1,35 @@
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../Hooks/useFetchGifs";
+// import { getGifs } from "../Helpers/GetGifs";
+
+
+export const GifGrid = ({ category }) => {
+
+    const{ images, isLoading } = useFetchGifs( category );
+    console.log({ isLoading });
+
+
+            
+    return (
+        <>
+            <h3>{ category }</h3>
+            {
+                isLoading && ( <h2>Cargando...</h2>)
+            }
+
+                <div className="card-grid">
+                    { 
+                        images.map( (image) => (
+                            <GifItem 
+                                key= {image.id} 
+                                // title= {image.title} 
+                                // url= {image.url} 
+                                { ...image }
+                            />
+                    ))
+                    }
+                </div>
+            
+        </>
+    )
+}
